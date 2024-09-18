c6d0eaabab4c24e02d36d4ee9d8203381dfe9ae6
Shalom Taiwo <ifeshalomt@gmail.com>
2024-09-18 13:19:51 +0200

    initial commit

--- src/config/config.js
+++ src/config/config.js
@@ -36,12 +36,7 @@ module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
-    options: {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
+    url: envVars.MONGODB_URL,
   },
   jwt: {
     secret: envVars.JWT_SECRET,
